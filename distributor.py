1a0ca062e8ed4efe391c5f21d2e4e79b81ea86f0
Omid Rezvani <orezvani@gmail.com>
2017-02-20 00:36:18 +1100

    Added minimum number of methods for the service handler code.

--- distributor.py
+++ distributor.py
@@ -2,3 +2,31 @@ class distributor:
 
     def __init__():
         return 0
+
+    
+    def print_table():
+        return 0
+
+
+    def add_job():
+        return 0
+
+
+    def check_servers_status():
+        return 0
+
+
+    def check_server_status():
+        return 0
+
+
+    def check_jobs_status():
+        return 0
+
+
+    def check_job_status():
+        return 0
+
+
+    def interface():
+        return 0
