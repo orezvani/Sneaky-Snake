f0e8afc940d6c62286e9346aadd2e65e10d2fec0
u5410055 <u5410055@anu.eud.au>
2017-03-07 14:04:08 +1100

    Created a minimal class for jobs.

--- job.py
+++ job.py
@@ -1,3 +1,19 @@
 class job:
-    __init__():
-        print "Created a job"
+
+    def __init__():
+        server_id = -1              # ID of the server that is taking care of this job
+        job_status = -1             # Status of this job based on what we checked last
+        last_check = -1             # Last time that we checked this job
+        queue_time = -1             # Time that it entered the queue
+        start_time = -1             # Start time for running of this job
+        end_time = -1               # Finish time for running this job
+        output = -1                 # Output of this job
+
+    """ Returns the status of this job """
+    def get_job_status():
+        return 0;
+
+    """ """
+    def run_job():
+        return 0
+
